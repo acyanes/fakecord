28d2832a26ec285b38c8d3c926ea3f2db7fd3c2d
aaron <acyanesswe@gmail.com>
2022-09-06 22:30:06 -0700

    started profile component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Server from './components/Server/Server';
 import ServerList from './components/Server/ServerList';
 import Login from './components/Home/Login';
 import './App.css';
+import Profile from './components/Home/Profile';
 
 function App() {
   // const [token, setToken] = useState();
@@ -16,7 +17,11 @@ function App() {
       <Server>
         <ServerList />
       </Server>
-      <div className='dm-column'>Direct Messages</div>
+      <div className='dm-column'>
+        <div>Direct Messages</div>
+        <Profile />
+      </div>
+
       <div className='message-column'>@ discordUserName</div>
     </div>
   );
--- src/components/Home/Profile.tsx
+++ src/components/Home/Profile.tsx
@@ -1,3 +1,16 @@
-const Profile = () => {};
+import ServerImage from '../Server/ServerImage';
+import '../../Profile.css';
+const Profile = () => {
+  return (
+    <div className='container'>
+      <div className='profile'>
+        <ServerImage />
+        {/* <div>online or not icon</div> */}
+        <div>ayeron</div>
+        <div>settings icon</div>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
